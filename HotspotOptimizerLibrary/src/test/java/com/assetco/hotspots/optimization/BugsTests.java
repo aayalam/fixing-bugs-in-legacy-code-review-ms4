e04936299da80f74b448c66857e0d1e066070285
Arnaldo Ayala <ayala@consultoresinformaticos.com.py>
2021-03-15 15:08:30 -0300

    Initial version, as downloaded from Manning LiveProject

---
+++ HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTests.java
@@ -0,0 +1,83 @@
+package com.assetco.hotspots.optimization;
+
+import com.assetco.hotspots.optimization.*;
+import com.assetco.search.results.*;
+import org.junit.jupiter.api.*;
+
+import java.math.*;
+import java.util.*;
+
+import static com.assetco.search.results.AssetVendorRelationshipLevel.*;
+import static com.assetco.search.results.HotspotKey.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class BugsTests {
+    private final int maximumShowcaseItems = 5;
+    private SearchResults searchResults;
+    private AssetVendor partnerVendor;
+    private SearchResultHotspotOptimizer optimizer;
+
+    @Test
+    public void prevailingPartnerReceivesFirstFiveItemsInShowcase() {
+        ArrayList<Asset> expected = new ArrayList<>();
+        expected.add(givenAssetInResultsWithVendor(partnerVendor));
+        givenAssetInResultsWithVendor(makeVendor(Partner));
+        expected.addAll(givenAssetsInResultsWithVendor(maximumShowcaseItems - 1, partnerVendor));
+
+        whenOptimize();
+
+        thenHotspotHasExactly(Showcase, expected);
+    }
+
+    @BeforeEach
+    public void setUp() {
+        optimizer = new SearchResultHotspotOptimizer();
+        searchResults = new SearchResults();
+        partnerVendor = makeVendor(Partner);
+    }
+
+    private AssetVendor makeVendor(AssetVendorRelationshipLevel relationshipLevel) {
+        return new AssetVendor("anything", "anything", relationshipLevel, 1);
+    }
+
+    private AssetPurchaseInfo getPurchaseInfoFromRoyaltiesAndRevenue(String royalties, String revenue) {
+        return new AssetPurchaseInfo(0, 0,
+                new Money(new BigDecimal(revenue)),
+                new Money(new BigDecimal(royalties)));
+    }
+
+    private Asset givenAssetInResultsWithVendor(AssetVendor vendor) {
+        Asset result = getAsset(vendor);
+        searchResults.addFound(result);
+        return result;
+    }
+
+    private Asset getAsset(AssetVendor vendor) {
+        return new Asset("anything", "anything", null, null, getPurchaseInfo(), getPurchaseInfo(), new ArrayList<>(), vendor);
+    }
+
+    private AssetPurchaseInfo getPurchaseInfo() {
+        return getPurchaseInfoFromRoyaltiesAndRevenue("0", "0");
+    }
+
+    private void thenHotspotHasExactly(HotspotKey hotspotKey, List<Asset> expected) {
+        Assertions.assertArrayEquals(expected.toArray(), searchResults.getHotspot(hotspotKey).getMembers().toArray());
+    }
+
+    private ArrayList<Asset> givenAssetsInResultsWithVendor(int count, AssetVendor vendor) {
+        var result = new ArrayList<Asset>();
+        for (var i = 0; i < count; ++i) {
+            result.add(givenAssetInResultsWithVendor(vendor));
+        }
+        return result;
+    }
+
+    private void whenOptimize() {
+        optimizer.optimize(searchResults);
+    }
+
+    private void thenHotspotDoesNotHave(HotspotKey key, Asset... forbidden) {
+        for (var asset : forbidden)
+            assertFalse(searchResults.getHotspot(key).getMembers().contains(asset));
+    }
+}
